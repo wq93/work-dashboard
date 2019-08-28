b733a003cb13a5f254f72f1254a2deb016fe318d
wangqi <wangqi@chicv.com>
2019-08-28 17:35:59 +0800

    content update

--- index.js
+++ index.js
@@ -1,3 +1,157 @@
+// 事业部列表
+var businessUnit = [
+  {
+    id: 1,
+    name: 'sw1'
+  },
+  {
+    id: 2,
+    name: 'sw2'
+  },
+  {
+    id: 3,
+    name: 'sw3'
+  },
+  {
+    id: 3,
+    name: 'sw4'
+  }
+]
+
+// todolist
+var todolist = [
+  {
+    iconPath: '', // icon图标链接
+    title: '待处理邮件', // 标题
+    finish: 1, // 已完成
+    total: 1, // 总共
+  },
+  {
+    title: '待处理邮件',
+    finish: 1,
+    total: 1
+  }
+]
+
+// 扇形图数据
+var lineData = [
+  { value: 335, name: '李阳-1' },
+  { value: 310, name: '李阳-2' },
+  { value: 234, name: '李阳-3' },
+  { value: 135, name: '李阳-4' },
+  { value: 1548, name: '李阳-5' },
+  { value: 1548, name: '李阳-6' },
+  { value: 1548, name: '李阳-7' },
+  { value: 1548, name: '李阳-8' },
+  { value: 1548, name: '李阳-9' },
+  { value: 1548, name: '李阳-10' },
+  { value: 1548, name: '李阳-11' },
+  { value: 1548, name: '李阳-12' },
+];
+
+// 柱状图数据
+var barData = [
+  {
+    name: '早上点外卖',
+    data: [
+      {
+        name: '李阳事业部-1',
+        value: 110,
+      },
+      {
+        name: '李阳事业部-2',
+        value: 111,
+      },
+      {
+        name: '李阳事业部-3',
+        value: 112,
+      },
+      {
+        name: '李阳事业部-4',
+        value: 113,
+      },
+      {
+        name: '李阳事业部-5',
+        value: 114,
+      }
+    ]
+  },
+  {
+    name: '中午点外卖',
+    data: [
+      {
+        name: '李阳事业部-1',
+        value: 120,
+      },
+      {
+        name: '李阳事业部-2',
+        value: 121,
+      },
+      {
+        name: '李阳事业部-3',
+        value: 122,
+      },
+      {
+        name: '李阳事业部-4',
+        value: 123,
+      },
+      {
+        name: '李阳事业部-5',
+        value: 124,
+      }
+    ]
+  },
+  {
+    name: '晚上点外卖',
+    data: [
+      {
+        name: '李阳事业部-1',
+        value: 130,
+      },
+      {
+        name: '李阳事业部-2',
+        value: 131,
+      },
+      {
+        name: '李阳事业部-3',
+        value: 132,
+      },
+      {
+        name: '李阳事业部-4',
+        value: 133,
+      },
+      {
+        name: '李阳事业部-5',
+        value: 134,
+      }
+    ]
+  },
+  {
+    name: '夜宵点外卖',
+    data: [
+      {
+        name: '李阳事业部-1',
+        value: 140,
+      },
+      {
+        name: '李阳事业部-2',
+        value: 141,
+      },
+      {
+        name: '李阳事业部-3',
+        value: 142,
+      },
+      {
+        name: '李阳事业部-4',
+        value: 143,
+      },
+      {
+        name: '李阳事业部-5',
+        value: 144,
+      }
+    ]
+  },
+]
 
 // 渲染柱状图
 var renderBarChart = function () {
@@ -19,63 +173,20 @@ var renderBarChart = function () {
     }
   };
 
-  var data = [
-    {
-      name: '新增订单数量',
-      data: {
-        'SW事业部': 200,
-        'JFN事业部': 120,
-        'Pi事业部': 20,
-        'NC事业部': 50,
-        'RW事业部': 10,
-      }
-    },
-    {
-      name: '新增邮件数量',
-      data: {
-        'SW事业部': 200,
-        'JFN事业部': 90,
-        'Pi事业部': 20,
-        'NC事业部': 50,
-        'RW事业部': 10,
-      }
-    },
-    {
-      name: '新增纠纷数量',
-      data: {
-        'SW事业部': 200,
-        'JFN事业部': 80,
-        'Pi事业部': 20,
-        'NC事业部': 50,
-        'RW事业部': 10,
-      }
-    },
-    {
-      name: '新增工单数量',
-      data: {
-        'SW事业部': 200,
-        'JFN事业部': 100,
-        'Pi事业部': 20,
-        'NC事业部': 50,
-        'RW事业部': 10,
-      }
-    },
-  ]
+  var categories = barData[ 0 ][ 'data' ].map(item => item.name);
 
-  var categories = Object.keys(data[0]['data']);
-
-  var series = data.map(item => {
+  var series = barData.map(item => {
     return {
       name: item.name,
       type: 'bar',
       barGap: 0,
       label: labelOption,
-      data: Object.values(item.data),
+      data: item.data.map(item => item.value),
     }
   })
 
   var option = {
-    color: ['#003366', '#006699', '#4cabce', '#e5323e'],
+    color: [ '#003366', '#006699', '#4cabce', '#e5323e' ],
     tooltip: {
       trigger: 'axis',
       axisPointer: {
@@ -83,7 +194,7 @@ var renderBarChart = function () {
       }
     },
     legend: {
-      data: ['Forest', 'Steppe', 'Desert', 'Wetland']
+      data: [ 'Forest', 'Steppe', 'Desert', 'Wetland' ]
     },
     toolbox: {
       show: true,
@@ -91,18 +202,18 @@ var renderBarChart = function () {
       left: 'right',
       top: 'center',
       feature: {
-        mark: {show: false},
-        dataView: {show: false, readOnly: false},
-        magicType: {show: false, type: ['bar']},
-        restore: {show: false},
-        saveAsImage: {show: false}
+        mark: { show: false },
+        dataView: { show: false, readOnly: false },
+        magicType: { show: false, type: [ 'bar' ] },
+        restore: { show: false },
+        saveAsImage: { show: false }
       }
     },
     calculable: true,
     xAxis: [
       {
         type: 'category',
-        axisTick: {show: false},
+        axisTick: { show: false },
         data: categories
       }
     ],
@@ -112,7 +223,7 @@ var renderBarChart = function () {
       }
     ],
     series: series,
-    backgroundColor: '#e9e9e9',
+    backgroundColor: '#f0f0f0',
   };
 
   var myChart = echarts.init(document.getElementById('bar-content'));
@@ -128,31 +239,25 @@ var renderBarChart = function () {
 }
 
 // 渲染扇形图
-var renderFanLineChart = function() {
+var renderFanLineChart = function () {
 
   var option = {
-    tooltip : {
+    tooltip: {
       trigger: 'item',
       formatter: "{a} <br/>{b} : {c} ({d}%)"
     },
     legend: {
       orient: 'vertical',
       left: 'left',
-      data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+      data: lineData.map(item => item.name)
     },
-    series : [
+    series: [
       {
         name: '访问来源',
         type: 'pie',
-        radius : '55%',
-        center: ['50%', '50%'],
-        data:[
-          {value:335, name:'直接访问'},
-          {value:310, name:'邮件营销'},
-          {value:234, name:'联盟广告'},
-          {value:135, name:'视频广告'},
-          {value:1548, name:'搜索引擎'}
-        ],
+        radius: '55%',
+        center: [ '50%', '50%' ],
+        data: lineData,
         itemStyle: {
           emphasis: {
             shadowBlur: 10,
@@ -162,7 +267,7 @@ var renderFanLineChart = function() {
         }
       }
     ],
-    backgroundColor: '#e9e9e9',
+    backgroundColor: '#f0f0f0',
   };
   var myChart = echarts.init(document.getElementById('fan-line-content'));
   myChart.showLoading({
@@ -175,6 +280,12 @@ var renderFanLineChart = function() {
     myChart.hideLoading();
   }, 800)
 }
+
+// 渲染事业部列表
+var renderDistributedList = function() {
+
+}
+
 
 renderBarChart();
 renderFanLineChart()
